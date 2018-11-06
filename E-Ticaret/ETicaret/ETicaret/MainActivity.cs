eef9344e8cd9036939599644e346ab520b88a827
SOFTSAM <sml.ozclk@gmail.com>
2018-11-06 23:06:13 +0300

    android project created
    Getting user info with UI
    Info Validation

---
+++ E-Ticaret/ETicaret/ETicaret/MainActivity.cs
@@ -0,0 +1,80 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Views.InputMethods;
+using Android.Views;
+
+namespace ETicaret
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText txtMail, txtPassword;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Button btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            btnLogin.Click += BtnLogin_Click;
+            txtMail = FindViewById<EditText>(Resource.Id.txtMail);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtPwd);
+            #region Enter Key pressed in edit texts
+            txtMail.KeyPress += (sender, e) => {
+                e.Handled = false;
+                if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+                {
+                    txtPassword.RequestFocus();
+                    //your logic here
+                    e.Handled = true;
+                }
+            };
+            txtPassword.KeyPress += (sender, e) => {
+                e.Handled = false;
+                if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+                {
+
+                    //your logic here
+                    btnLogin.PerformClick();
+                    e.Handled = true;
+                }
+            };
+            #endregion
+
+        }
+
+        private void BtnLogin_Click(object sender, System.EventArgs e)
+        {
+            //Make validations and connect to database and check is this user is exists and infos are correct
+            if (ValidEMail(txtMail.Text)&&ValidPassWord(txtPassword.Text))
+            {
+                //this is a test validation
+                //email is valid
+                MessageBox("Valid email and password");
+            }
+            else
+            {
+                MessageBox("Invalid email or password");
+            }
+        }
+        private void MessageBox(string text)
+        {
+            Toast.MakeText(this, text, ToastLength.Short).Show();
+        }
+
+        private bool ValidPassWord(string text)
+        {
+            return text.Length >= 8;
+        }
+
+        private bool ValidEMail(string text)
+        {
+            return text.Contains("@");
+        }
+    }
+}
